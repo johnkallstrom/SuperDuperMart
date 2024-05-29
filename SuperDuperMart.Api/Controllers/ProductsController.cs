5a55bd417097f9361aab086bd66ae918afd7d3f7
John Källström <johnkaellstrom@gmail.com>
2024-05-29 13:39:16 +0200

    Add project files.

---
+++ SuperDuperMart.Api/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperDuperMart.Api.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ SuperDuperMart.Core/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperDuperMart.Core.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Username { get; set; } = default!;
+        public string Email { get; set; } = default!;
+
+        public Address Address { get; set; } = default!;
+    }
+}
