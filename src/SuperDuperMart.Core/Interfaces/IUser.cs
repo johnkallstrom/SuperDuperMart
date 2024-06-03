4f7a1df2289c3aa5485b79f5efcc407872e2f2db
John Källström <johnkaellstrom@gmail.com>
2024-06-03 13:44:03 +0200

    Add user interface

---
+++ src/SuperDuperMart.Core/Interfaces/IUser.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperDuperMart.Core.Interfaces
+{
+    public interface IUser
+    {
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
