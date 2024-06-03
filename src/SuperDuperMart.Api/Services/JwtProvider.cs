7a5451cf4790fa122aafd301d522cb3399ecae5e
John Källström <johnkaellstrom@gmail.com>
2024-06-03 17:28:56 +0200

    Generate jwt token

--- src/SuperDuperMart.Api/Services/JwtProvider.cs
+++ src/SuperDuperMart.Api/Services/JwtProvider.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SuperDuperMart.Api.Models;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SuperDuperMart.Api.Services
 {
@@ -15,7 +18,25 @@ namespace SuperDuperMart.Api.Services
 
         public string GenerateToken(User user)
         {
-            var token = new JwtSecurityToken();
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer, 
+                audience: _jwtSettings.Audience, 
+                claims: claims, 
+                notBefore: null, 
+                expires: DateTime.Now.AddMinutes(60), 
+                signingCredentials: signingCredentials);
+
             var handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);
         }
