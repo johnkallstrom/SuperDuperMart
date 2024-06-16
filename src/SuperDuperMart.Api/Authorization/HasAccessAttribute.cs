6c999d6f58a31a2e834abeb4aa9f19983c4a5433
John Källström <johnkaellstrom@gmail.com>
2024-06-16 14:04:59 +0200

    Custom authorization attribute

---
+++ src/SuperDuperMart.Api/Authorization/HasAccessAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SuperDuperMart.Api.Authorization
+{
+    public class HasAccessAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
