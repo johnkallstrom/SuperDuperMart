c69c6ef0cb142683a4b046b1d6d448309bb82773
John Källström <johnkaellstrom@gmail.com>
2024-07-03 12:07:48 +0200

    Navbar

--- src/SuperDuperMart.Web/Program.cs
+++ src/SuperDuperMart.Web/Program.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SuperDuperMart.Web;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
+var host = builder.Build();
+await host.RunAsync();
