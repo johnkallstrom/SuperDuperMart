86ebf5134ee23741d8199d354ac56c795013831a
John Källström <johnkaellstrom@gmail.com>
2024-07-29 15:19:37 +0200

    Edit and Delete pages

---
+++ src/SuperDuperMart.Web/Features/Products/Delete.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using SuperDuperMart.Web.Http;
+
+namespace SuperDuperMart.Web.Features.Products
+{
+    public partial class Delete
+    {
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        public bool Loading { get; set; }
+        public ProductModel? Model { get; set; } = default!;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await GetProduct();
+        }
+
+        private async Task GetProduct()
+        {
+            Loading = true;
+            Model = await HttpService.GetAsync<ProductModel>($"{Endpoints.Products}/{Id}");
+            Loading = false;
+        }
+    }
+}
