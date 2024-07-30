ad3203ac49b8a28fd09669bd55f08af34bb8be6e
John Källström <johnkaellstrom@gmail.com>
2024-07-30 22:47:13 +0200

    Create product

--- src/SuperDuperMart.Web/Features/Administrators/Products/Create.razor.cs
+++ src/SuperDuperMart.Web/Features/Administrators/Products/Create.razor.cs
@@ -1,6 +1,24 @@
-﻿namespace SuperDuperMart.Web.Features.Administrators.Products
+﻿using Microsoft.AspNetCore.Components;
+using SuperDuperMart.Web.Http;
+
+namespace SuperDuperMart.Web.Features.Administrators.Products
 {
     public partial class Create
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
+        public ProductCreateModel Model { get; set; } = new();
+
+        private async Task Submit()
+        {
+            await HttpService.PostAsync($"{Endpoints.Products}", Model);
+            NavigationManager.NavigateTo("/manage/products");
+        }
+
+        private void Cancel() => NavigationManager.NavigateTo("/manage/products");
     }
 }
