bcb516f016698d76ed50534fd80661becc5e9b03
John Källström <johnkaellstrom@gmail.com>
2024-07-31 20:36:45 +0200

    Display products and shopping cart page

---
+++ src/SuperDuperMart.Web/Features/Customers/Carts/Index.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuperDuperMart.Web.Features.Customers.Carts
+{
+    public partial class Index
+    {
+        [Parameter]
+        public int UserId { get; set; }
+    }
+}
---
+++ src/SuperDuperMart.Web/Features/Customers/Products/Components/ProductCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuperDuperMart.Web.Features.Customers.Products.Components
+{
+    public partial class ProductCard
+    {
+        [Parameter, EditorRequired]
+        public ProductModel Product { get; set; } = default!;
+    }
+}
