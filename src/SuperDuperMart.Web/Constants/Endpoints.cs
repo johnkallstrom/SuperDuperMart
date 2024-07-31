cf5a38d1d156dc86f72bbf2b1f275d5a450fcddd
John Källström <johnkaellstrom@gmail.com>
2024-07-31 21:23:00 +0200

    Get cart from frontend

--- src/SuperDuperMart.Web/Constants/Endpoints.cs
+++ src/SuperDuperMart.Web/Constants/Endpoints.cs
@@ -3,6 +3,7 @@
     public static class Endpoints
     {
         public const string Authentication = "/api/authentication";
+        public const string Carts = "/api/carts";
         public const string Products = "/api/products";
         public const string Users = "/api/users";
     }
--- src/SuperDuperMart.Web/Features/Customers/Carts/Index.razor.cs
+++ src/SuperDuperMart.Web/Features/Customers/Carts/Index.razor.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Components;
+using SuperDuperMart.Shared.Models.Carts;
 
 namespace SuperDuperMart.Web.Features.Customers.Carts
 {
     public partial class Index
     {
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
         [Parameter]
         public int UserId { get; set; }
+
+        public CartModel? Model { get; set; } = default!;
+        public bool Loading { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Loading = true;
+            await GetCart();
+            Loading = false;
+        }
+
+        private async Task GetCart()
+        {
+            Model = await HttpService.GetAsync<CartModel>($"{Endpoints.Carts}/user/{UserId}");
+        }
     }
 }
