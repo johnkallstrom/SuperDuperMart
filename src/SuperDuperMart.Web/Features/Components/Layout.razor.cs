31ba79a0b176cc3f579ae9191b71aac70ec7bdb4
John Källström <johnkaellstrom@gmail.com>
2024-07-31 09:47:00 +0200

    Spinner on application startup

---
+++ src/SuperDuperMart.Web/Features/Components/Layout.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace SuperDuperMart.Web.Features.Components
+{
+    public partial class Layout
+    {
+        private ErrorBoundary? errorBoundary;
+
+        protected override void OnParametersSet()
+        {
+            errorBoundary?.Recover();
+        }
+
+        protected override void OnInitialized()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
