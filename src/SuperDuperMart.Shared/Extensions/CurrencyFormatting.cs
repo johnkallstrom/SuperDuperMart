b56b71f1750ec45329259470f5ed9b5023756b3b
John Källström <johnkaellstrom@gmail.com>
2024-07-31 20:36:34 +0200

    Extensions

---
+++ src/SuperDuperMart.Shared/Extensions/CurrencyFormatting.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace SuperDuperMart.Shared.Extensions
+{
+    public static class CurrencyFormatting
+    {
+        public static string ToCurrency(this decimal value, string culture)
+        {
+            return value.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE"));
+        }
+    }
+}
--- src/SuperDuperMart.Shared/Extensions/DateTimeFormatting.cs
+++ src/SuperDuperMart.Shared/Extensions/DateTimeFormatting.cs
@@ -2,12 +2,7 @@
 {
     public static class DateTimeFormatting
     {
-        /// <summary>
-        /// Use for custom datetime formatting that will looks something like this, 31-10-2024 15:32:14
-        /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public static string ToDateTimeFormat(this DateTime date)
+        public static string ToSpecifiedDateFormat(this DateTime date)
         {
             return date.ToString("dd-MM-yyyy HH:mm:ss");
         }
