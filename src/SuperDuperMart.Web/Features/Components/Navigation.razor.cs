b8e1f2234dfcc6889590003b77dc24352a9dcbc7
John Källström <johnkaellstrom@gmail.com>
2024-08-01 18:15:34 +0200

    Check auth state in nav component

--- src/SuperDuperMart.Web/Features/Components/Navigation.razor.cs
+++ src/SuperDuperMart.Web/Features/Components/Navigation.razor.cs
@@ -1,10 +1,15 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace SuperDuperMart.Web.Features.Components
 {
     public partial class Navigation
     {
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
 
@@ -14,10 +19,29 @@ namespace SuperDuperMart.Web.Features.Components
         //[Inject]
         //public ISessionStorageService SessionStorage { get; set; } = default!;
 
+        protected override void OnInitialized()
+        {
+            AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateChanged;
+            NavigationManager.LocationChanged += LocationChanged;
+        }
+
+        private async void AuthenticationStateChanged(Task<AuthenticationState> task)
+        {
+            var state = await task;
+            Console.WriteLine($"Is Authenticated: {state.User.Identity?.IsAuthenticated}");
+        }
+
+        private void LocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            string locationChangedBy = e.IsNavigationIntercepted ? "HTML" : "Code";
+            Console.WriteLine($"Location: {e.Location}");
+            Console.WriteLine($"Changed By: {locationChangedBy}");
+        }
+
         private async Task Logout()
         {
             await LocalStorage.RemoveItemAsync("token");
-            NavigationManager.NavigateTo("/", forceLoad: true);
+            NavigationManager.NavigateTo("/");
         }
     }
 }
