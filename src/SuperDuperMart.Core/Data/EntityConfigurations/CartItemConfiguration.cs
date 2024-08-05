949ab21d4064dc238f4917330100df56abe9fd88
John Källström <johnkaellstrom@gmail.com>
2024-08-05 14:46:22 +0200

    Cart item entity

---
+++ src/SuperDuperMart.Core/Data/EntityConfigurations/CartItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SuperDuperMart.Core.Data.EntityConfigurations
+{
+    public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.ToTable("CartItem");
+        }
+    }
+}
---
+++ src/SuperDuperMart.Core/Entities/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperDuperMart.Core.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = default!;
+
+        public int CartId { get; set; }
+        public Cart Cart { get; set; } = default!;
+
+        public int Quantity { get; set; }
+    }
+}
