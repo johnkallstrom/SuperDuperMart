87a1da12921d787d90dc8c53aabf177624982cfb
John Källström <johnkaellstrom@gmail.com>
2024-10-11 14:45:04 +0200

    Parameters

--- src/SuperDuperMart.Web/Features/Administrators/Users/Edit.razor.cs
+++ src/SuperDuperMart.Web/Features/Administrators/Users/Edit.razor.cs
@@ -1,6 +1,10 @@
-﻿namespace SuperDuperMart.Web.Features.Administrators.Users
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuperDuperMart.Web.Features.Administrators.Users
 {
     public partial class Edit
     {
+        [Parameter]
+        public int Id { get; set; }
     }
 }
