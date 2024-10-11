228e91aa8d33c9a9290d3c3109da147847a9dd8b
John Källström <johnkaellstrom@gmail.com>
2024-10-11 15:38:12 +0200

    Models

---
+++ src/SuperDuperMart.Shared/Models/Users/LocationUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperDuperMart.Shared.Models.Users
+{
+    public record LocationUpdateModel
+    {
+        public string? ZipCode { get; set; }
+        public string? StreetName { get; set; }
+        public string? City { get; set; }
+    }
+}
---
+++ src/SuperDuperMart.Shared/Models/Users/UserUpdateModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperDuperMart.Shared.Models.Users
+{
+    public record UserUpdateModel
+    {
+        public string? Avatar { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public LocationUpdateModel Location { get; set; } = new();
+
+        [Required(ErrorMessage = "Please enter username")]
+        public string Username { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please enter email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please enter valid password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please enter valid confirm password")]
+        [Compare(nameof(Password), ErrorMessage = "Please provide matching passwords")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = default!;
+    }
+}
