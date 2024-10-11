f006e86fb67e5e685e10527060fd9d7076979246
John Källström <johnkaellstrom@gmail.com>
2024-10-11 08:55:28 +0200

    User create form

--- src/SuperDuperMart.Shared/Models/Users/UserCreateModel.cs
+++ src/SuperDuperMart.Shared/Models/Users/UserCreateModel.cs
@@ -5,20 +5,25 @@ namespace SuperDuperMart.Shared.Models.Users
     public record UserCreateModel
     {
         [Required(ErrorMessage = "Please enter a firstname")]
-        public string FirstName { get; init; } = default!;
+        public string FirstName { get; set; } = default!;
 
         [Required(ErrorMessage = "Please enter a lastname")]
-        public string LastName { get; init; } = default!;
+        public string LastName { get; set; } = default!;
 
-        public string? Username { get; init; }
+        public string? Username { get; set; }
 
         [Required(ErrorMessage = "Please enter a email address")]
         [DataType(DataType.EmailAddress)]
-        public string Email { get; init; } = default!;
+        public string Email { get; set; } = default!;
 
         [Required(ErrorMessage = "Please enter a valid password")]
         [DataType(DataType.Password)]
-        public string Password { get; init; } = default!;
+        public string Password { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please enter a valid confirm password")]
+        [Compare("Password", ErrorMessage = "Please provide matching passwords")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = default!;
 
         public LocationCreateModel Location { get; set; } = default!;
     }
--- src/SuperDuperMart.Web/Features/Administrators/Users/Create.razor.cs
+++ src/SuperDuperMart.Web/Features/Administrators/Users/Create.razor.cs
@@ -1,6 +1,24 @@
-﻿namespace SuperDuperMart.Web.Features.Administrators.Users
+﻿using Microsoft.AspNetCore.Components;
+using SuperDuperMart.Shared.Models.Users;
+
+namespace SuperDuperMart.Web.Features.Administrators.Users
 {
     public partial class Create
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
+        public UserCreateModel Model { get; set; } = new();
+
+        private async Task Submit()
+        {
+            await HttpService.PostAsync($"{Endpoints.Users}", Model);
+            NavigationManager.NavigateTo("/manage/users");
+        }
+
+        private void Cancel() => NavigationManager.NavigateTo("/manage/users");
     }
 }
