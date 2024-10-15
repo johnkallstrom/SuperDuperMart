df8bed787944d75faeb9d81aeabf6e4bb93e535c
John Källström <johnkaellstrom@gmail.com>
2024-10-15 12:52:43 +0200

    Render Fragments

--- src/SuperDuperMart.Web/Features/Components/Collapse.razor.cs
+++ src/SuperDuperMart.Web/Features/Components/Collapse.razor.cs
@@ -5,6 +5,9 @@ namespace SuperDuperMart.Web.Features.Components
     public partial class Collapse
     {
         [Parameter]
-        public RenderFragment ChildContent { get; set; } = default!;
+        public RenderFragment Header { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment Body { get; set; } = default!;
     }
 }
