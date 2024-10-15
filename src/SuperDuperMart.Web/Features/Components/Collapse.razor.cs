b8129024793b231af9b144eef33f27242fd00ace
John Källström <johnkaellstrom@gmail.com>
2024-10-15 11:29:32 +0200

    Testing render fragments

---
+++ src/SuperDuperMart.Web/Features/Components/Collapse.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuperDuperMart.Web.Features.Components
+{
+    public partial class Collapse
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; } = default!;
+    }
+}
