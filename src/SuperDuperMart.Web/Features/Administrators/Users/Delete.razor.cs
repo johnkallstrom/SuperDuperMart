058feb0526cc45a1e09952ef4c227c9d282142f2
John Källström <johnkaellstrom@gmail.com>
2024-10-31 09:05:15 +0100

    Delete user api call from frontend

--- src/SuperDuperMart.Web/Features/Administrators/Users/Delete.razor.cs
+++ src/SuperDuperMart.Web/Features/Administrators/Users/Delete.razor.cs
@@ -4,7 +4,35 @@ namespace SuperDuperMart.Web.Features.Administrators.Users
 {
     public partial class Delete
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
         [Parameter]
         public int Id { get; set; }
+
+        private bool _loading = true;
+        public UserModel? Model { get; set; } = default!;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await GetUser();
+        }
+
+        private async Task GetUser()
+        {
+            Model = await HttpService.GetAsync<UserModel>($"{Endpoints.Users}/{Id}");
+            _loading = false;
+        }
+
+        private async Task DeleteUser()
+        {
+            await HttpService.DeleteAsync($"{Endpoints.Products}/{Id}");
+            NavigationManager.NavigateTo("/manage/users");
+        }
+
+        private void Cancel() => NavigationManager.NavigateTo("/manage/users");
     }
 }
