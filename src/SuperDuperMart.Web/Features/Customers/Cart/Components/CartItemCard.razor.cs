75fa1ca11dc0dc8e04da9af0d3470c5d52a2c52f
John Källström <johnkaellstrom@gmail.com>
2024-11-05 14:07:55 +0100

    Delete cart item

--- src/SuperDuperMart.Web/Features/Customers/Cart/Components/CartItemCard.razor.cs
+++ src/SuperDuperMart.Web/Features/Customers/Cart/Components/CartItemCard.razor.cs
@@ -5,7 +5,18 @@ namespace SuperDuperMart.Web.Features.Customers.Cart.Components
 {
     public partial class CartItemCard
     {
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
         [Parameter, EditorRequired]
         public CartItemModel Item { get; set; } = default!;
+
+        private async Task DeleteCartItem()
+        {
+            int cartId = 1;
+            int productId = Item.Product.Id;
+
+            await HttpService.DeleteAsync($"{Endpoints.Carts}/{cartId}/items/delete/{productId}");
+        }
     }
 }
