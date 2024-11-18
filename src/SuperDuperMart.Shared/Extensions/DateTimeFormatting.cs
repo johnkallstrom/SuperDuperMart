4eb332e1a1a5d73dcbc1f4125640040df93e3d3a
John Källström <johnkaellstrom@gmail.com>
2024-11-18 17:08:25 +0100

    Date formatting

--- src/SuperDuperMart.Shared/Extensions/DateTimeFormatting.cs
+++ src/SuperDuperMart.Shared/Extensions/DateTimeFormatting.cs
@@ -6,5 +6,10 @@
         {
             return date.ToString("dd-MM-yyyy HH:mm:ss");
         }
+
+        public static string ToDateFormat(this DateTime date)
+        {
+            return date.ToString("dd-MM-yyyy");
+        }
     }
 }
