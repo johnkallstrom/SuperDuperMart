9c0440d1ac6760d26897b8c2ce7b7cdde8c7623c
John Källström <johnkaellstrom@gmail.com>
2024-11-26 17:54:28 +0100

    Sorting

--- src/SuperDuperMart.Web/Features/Members/Products/Index.razor.cs
+++ src/SuperDuperMart.Web/Features/Members/Products/Index.razor.cs
@@ -13,6 +13,8 @@ namespace SuperDuperMart.Web.Features.Members.Products
 
         private bool _loading = true;
 
+        public string SortBy { get; set; } = "Created";
+        public string SortOrder { get; set; } = "Desc";
         public PagedListDto<ProductDto> Model { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
@@ -22,12 +24,25 @@ namespace SuperDuperMart.Web.Features.Members.Products
 
             Model = new(pageNumber, pageSize);
 
-            await GetProducts(Model.PageNumber, Model.PageSize);
+            await GetProducts(Model.PageNumber, Model.PageSize, SortBy, SortOrder);
         }
 
-        private async Task GetProducts(int pageNumber, int pageSize)
+        private async Task GetProducts(
+            int pageNumber, 
+            int pageSize, 
+            string? sortBy, 
+            string? sortOrder)
         {
-            string? url = $"{Endpoints.Products}?pageNumber={pageNumber}&pageSize={pageSize}";
+            string? url = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(SortBy) && !string.IsNullOrWhiteSpace(SortOrder))
+            {
+                url = $"{Endpoints.Products}?pageNumber={pageNumber}&pageSize={pageSize}&sortBy={SortBy}&sortOrder={SortOrder}";
+            }
+            else
+            {
+                url = $"{Endpoints.Products}?pageNumber={pageNumber}&pageSize={pageSize}";
+            }
 
             var result = await HttpService.GetAsync<PagedListDto<ProductDto>>(url);
             if (result != null)
@@ -38,16 +53,21 @@ namespace SuperDuperMart.Web.Features.Members.Products
             _loading = false;
         }
 
+        private void HandleSortChange(ChangeEventArgs args)
+        {
+            Console.WriteLine(args);
+        }
+
         private async Task HandlePreviousClick(int pageNumber)
         {
             Model.PageNumber = pageNumber;
-            await GetProducts(Model.PageNumber, Model.PageSize);
+            await GetProducts(Model.PageNumber, Model.PageSize, SortBy, SortOrder);
         }
 
         private async Task HandleNextClick(int pageNumber)
         {
             Model.PageNumber = pageNumber;
-            await GetProducts(Model.PageNumber, Model.PageSize);
+            await GetProducts(Model.PageNumber, Model.PageSize, SortBy, SortOrder);
         }
     }
 }
