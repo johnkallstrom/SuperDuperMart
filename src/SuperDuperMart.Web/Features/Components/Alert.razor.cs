3aeb646ecc2f894d9053ecda3e7c927d4d6ba057
John Källström <johnkaellstrom@gmail.com>
2024-11-30 09:53:00 +0100

    Global error handling with alert

---
+++ src/SuperDuperMart.Web/Features/Components/Alert.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SuperDuperMart.Web.Features.Components
+{
+    public partial class Alert
+    {
+        [Parameter, EditorRequired]
+        public string Message { get; set; } = default!;
+
+        [Parameter]
+        public Color Color { get; set; }
+    }
+}
