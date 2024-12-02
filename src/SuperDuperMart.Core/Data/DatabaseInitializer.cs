da393b9065235c4d19d1d28dce0e1d4d3c2297d4
John Källström <johnkaellstrom@gmail.com>
2024-12-02 17:49:21 +0100

    Seed data

--- src/SuperDuperMart.Core/Data/DatabaseInitializer.cs
+++ src/SuperDuperMart.Core/Data/DatabaseInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SuperDuperMart.Core.Data.Fakers;
 
@@ -20,19 +21,50 @@ namespace SuperDuperMart.Core.Data
             UserManager<User> userManager, 
             RoleManager<Role> roleManager)
         {
-            var products = _productFaker.Generate(100);
+            await AddProductCategories(context);
+            await AddProducts(context);
+            await AddUsers(userManager, roleManager, configuration);
+        }
 
-            if (products != null && products.Count > 0)
+        private static async Task AddProductCategories(SuperDuperMartDbContext context)
+        {
+            var categoryNames = _faker.Commerce
+              .Categories(100)
+              .Distinct();
+
+            var productCategories = categoryNames.Select(name => new ProductCategory
             {
-                await context.Products.AddRangeAsync(products);
+                Name = name,
+                Description = _faker.Lorem.Paragraph()
+            }).ToList();
+
+            if (productCategories != null && productCategories.Count > 0)
+            {
+                await context.ProductCategories.AddRangeAsync(productCategories);
             }
 
             await context.SaveChangesAsync();
-
-            await CreateUsers(userManager, roleManager, configuration);
         }
 
-        private static async Task CreateUsers(
+        private static async Task AddProducts(SuperDuperMartDbContext context)
+        {
+            var products = _productFaker.Generate(100);
+            if (products != null && products.Count > 0)
+            {
+                var categories = await context.ProductCategories.ToListAsync();
+
+                foreach (var product in products)
+                {
+                    var randomCategory = _faker.PickRandom(categories);
+                    product.CategoryId = randomCategory.Id;
+
+                    await context.Products.AddAsync(product);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        private static async Task AddUsers(
             UserManager<User> userManager, 
             RoleManager<Role> roleManager, 
             IConfiguration configuration)
