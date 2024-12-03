a577ed19f0ea5dfd194ff05059884cfb9ebe5a64
John Källström <johnkaellstrom@gmail.com>
2024-12-03 16:06:22 +0100

    Extensions

---
+++ src/SuperDuperMart.Web/Extensions/SelectOptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SuperDuperMart.Web.Extensions
+{
+    public static class SelectOptionExtensions
+    {
+        /// <summary>
+        /// Map RoleDto to SelectOption with 'Name' as text and 'Id' as value.
+        /// </summary>
+        /// <param name="dtos"></param>
+        /// <returns></returns>
+        public static List<SelectOption> ToSelectOptionList(this IEnumerable<RoleDto> dtos)
+        {
+            ArgumentNullException.ThrowIfNull(dtos);
+
+            return dtos
+                .Select(d => new SelectOption(d.Name, d.Id.ToString()))
+                .ToList();
+        }
+    }
+}
